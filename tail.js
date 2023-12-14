c2b07ae82c0017457d1e61fadc08e1d609922217
EdmoreMafuwe <126406684+EdmoreMafuwe@users.noreply.github.com>
2023-12-13 23:54:39 -0800

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,5 +1,10 @@
 const tail = function(array) {
-  return array.slice(1);
+  let tailArray = [];
+  for (let i = 1; i < array.length; i ++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
 };
+
 
 module.exports = tail;
